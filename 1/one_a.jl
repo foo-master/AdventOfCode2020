6d5795cbdaaf1761832ecc2f46f1e84081cc9d8f
Christoph Karle <christoph.karle@leica-geosystems.com>
2020-12-03 10:40:25 +0100

    Puzzle 1: Add functional version

--- 1/one_a.jl
+++ 1/one_a.jl
@@ -1,19 +1,34 @@
-lines = open("one.txt", "r") do f
+using BenchmarkTools
+
+cd(dirname(@__FILE__))
+const lines = open("one.txt", "r") do f
     readlines(f)
 end
 
-ints = map(lines) do l
+ints = map(lines) do l     # don't make this const, otherwise @btime does not give valid results
     parse(Int64, l)
 end
+const len = length(ints)   # but this can be const and makes a big difference for the benchmarks
 
-len = length(ints)
-for i in 1:len
+x = y = 0
+@btime for i in 1:len
     for j in i+1:len
-        x = ints[i]
-        y = ints[j]
-        if x+y == 2020
-            println(x," ", y, " ", x+y, " ", x*y)
+        if ints[i]+ints[j] == 2020
+            global x = ints[i]
+            global y = ints[j]
             break
         end
     end
 end
+println(x," ", y, " ", x+y, " ", x*y)
+
+# More functional-style version that uses list comprehensions as generators for the indices
+pair_product(a,b) = ((i,j) for i in a:b for j in i+1:b)
+
+x = y = 0
+@btime begin
+    (i,j) = first((i,j) for (i,j) in pair_product(1, len) if ints[i]+ints[j] == 2020)
+    global x = ints[i]
+    global y = ints[j]
+end
+println(x," ", y, " ", x+y, " ", x*y)
--- 1/one_b.jl
+++ 1/one_b.jl
@@ -1,22 +1,40 @@
-lines = open("one.txt", "r") do f
+using BenchmarkTools
+
+cd(dirname(@__FILE__))
+const lines = open("one.txt", "r") do f
     readlines(f)
 end
 
-ints = map(lines) do l
+ints = map(lines) do l     # don't make this const, otherwise @btime does not give valid results
     parse(Int64, l)
 end
+const len = length(ints)   # but this can be const and makes a big difference for the benchmarks
 
-len = length(ints)
-for i in 1:len
+x = y = z = 0
+@btime for i in 1:len
     for j in i+1:len
         for k in j+1:len
-            x = ints[i]
-            y = ints[j]
-            z = ints[k]
-            if x+y+z == 2020
-                println(x," ", y, " ", z, " ", x+y+z, " ", x*y*z)
+            if ints[i]+ints[j]+ints[k] == 2020
+                global x = ints[i]
+                global y = ints[j]
+                global z = ints[k]
                 break
             end
         end
     end
 end
+
+println(x," ", y, " ", z, " ", x+y+z, " ", x*y*z)
+
+# More functional-style version that uses list comprehensions as generators for the indices
+pair_product(a,b) = ((i,j) for i in a:b for j in i+1:b)
+triple_product(a,b) = ((i,j,k) for i in a:b for (j,k) in pair_product(i+1,b))
+
+x = y = z = 0
+@btime begin
+    (i,j,k) = first((i,j,k) for (i,j,k) in triple_product(1, len) if ints[i]+ints[j]+ints[k] == 2020)
+    global x = ints[i]
+    global y = ints[j]
+    global z = ints[k]
+end
+println(x," ", y, " ", z, " ", x+y+z, " ", x*y*z)
